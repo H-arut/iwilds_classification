b7da1c07c5a21ee119a4d4427e7145be6ac7b154
Grizzly001 <ameyroyan@yahoo.com>
2023-02-04 20:59:20 +0400

    Kapi stugum

--- source/readdata.py
+++ source/readdata.py
@@ -1,6 +1,6 @@
-from wilds import get_dataset
-from wilds.common.data_loaders import get_train_loader
-import torchvision.transforms as transforms
+# from wilds import get_dataset
+# from wilds.common.data_loaders import get_train_loader
+# import torchvision.transforms as transforms
 
 # Load the full dataset, and download it if necessary
 dataset = get_dataset(dataset="iwildcam", download=True)
@@ -18,3 +18,7 @@ train_loader = get_train_loader("standard", train_data, batch_size=16)
 
 for labeled_batch in train_loader:
     x, y, metadata = labeled_batch
+
+
+
+#Barev dzez
